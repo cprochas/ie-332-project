3b309acf66c96736478448ca2509a0cc950da8a6
cprochas <48108375+cprochas@users.noreply.github.com>
2019-04-02 17:21:40 -0400

    Create admin2.php

---
+++ admin2.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html>
+   <head>
+      <title>Admin2</title>
+     <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>	    
+</head>
+
+<body background= "giraffe.jpg";> <!-- style="background-color:lime;"> -->
+
+
+<div id="main">
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<center><font size="6"> Please Select One Of The Following Options  </center>
+	<br>
+	<br>
+	<form action="https://web.ics.purdue.edu/~g1100782/Costs.php" style="text-align: center;">
+    <input type="submit" class="btn btn-primary" value="Costs";/>
+	</form>
+	<br>
+	<form action="https://web.ics.purdue.edu/~g1100782/Expenditure%20&%20Budget.php?" style="text-align: center;">
+    <input type="submit" class="btn btn-primary" value="Budgets";/>
+	</form>
+	<br>
+	<form action="https://web.ics.purdue.edu/~g1100782/Patient%20Portal.php?" style="text-align: center;">
+    <input type="submit" class="btn btn-primary" value="Patient Portal" />
+	</form>
+	<br>
+	<form action="https://web.ics.purdue.edu/~g1100782/Inventory%20Management.php?" style="text-align: center;">
+    <input type="submit" class="btn btn-primary" value="Inventory Management";/>
+	</form>
+	<br>
+	<form action="https://web.ics.purdue.edu/~g1100782/Purchasing%20Trends.php?" style="text-align: center;">
+    <input type="submit" class="btn btn-primary" value="Purchasing Trends" />
+	</form>
+	<br>
+	<form action="https://web.ics.purdue.edu/~g1100782/Requests.php?" style="text-align: center;">
+    <input type="submit" class="btn btn-primary" value="Requests" />
+	</form>
+	<br>
+</div>
+</body>
+</html>
