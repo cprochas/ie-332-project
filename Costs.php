14723b47cca4a0e228d4bd14369a4fc48e4ca622
cprochas <48108375+cprochas@users.noreply.github.com>
2019-04-02 17:22:07 -0400

    Create Costs.php

---
+++ Costs.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html>
+   <head>
+      <title>Costs</title>
+     <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>	    
+</head>
+
+<body background= "giraffe.jpg";> <!-- style="background-color:lime;"> -->
+
+
+<div id="main">
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<center><font size="6"> Costs  </center>
+	
+</div>
+</body>
+</html>
