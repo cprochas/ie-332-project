5384fb25937d9e384c578eece4206e2abba8d50f
cprochas <48108375+cprochas@users.noreply.github.com>
2019-04-02 17:23:43 -0400

    Create login.php

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html>
+   <head>
+      <title>Sign In</title>
+      <link href="login.css" rel="stylesheet" type="text/css" media="screen" />			    
+</head>
+
+<body>
+	<div id="main">
+	<div id="img">
+    		   <img src="ugandaseal.svg" width= "150" height="200" align="center">
+		   </div>
+	<p class="sign" align="center">Sign In</p>
+		<form action="/admin.php">
+			<input class="pass" type="text" align="center" placeholder="Username">
+		<br>
+			<input class="pass" type="password" align="center" placeholder="Password">
+		<br><br>
+		<a class="submit" align="center"> Sign In</a>
+		</form>
+ 
+    </div>
+	<div style="clear:both"></div>
+</div>
+</body>
+</html>
